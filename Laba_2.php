ce7cddffcca2a7fa40777840646ca5de0ca1ccea
valer <valeri.tga@gmail.com>
2021-10-18 22:04:25 +0200

    Laba_2.php: Создание нового php файла, в котором выводятся фильмы по возрасту

---
+++ Laba_2.php
@@ -0,0 +1,7 @@
+<?php
+echo "Введите ваш возраст: ";
+$age = readline();
+$movies=["The Shawshank Redemption (1992), 16+. Rating - 9.3", "The Godfather (1972), 16+. Rating - 9.2", "The Dark Knight (2008), 14+. Rating - 9", "12 Angry Men (1957), 16+. Rating - 9", "Schindler's List (1993), 16+. Rating - 8.9"];
+echo is_int($age);
+
+?>
